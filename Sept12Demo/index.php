abec5af4fb63c8a84a51c35480752f220297df9e
Scott Rafael <w0218584@nscc.ca>
2016-09-13 18:34:49 -0300

    Sept12 Demo recreation

---
+++ Sept12Demo/index.php
@@ -0,0 +1,20 @@
+<h1>Hello, from Scott's PHP Page</h1>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 9/13/16
+ * Time: 6:27 PM
+ */
+echo "<p>Paragraph Text</p>";
+
+
+?>
+<h2>Not a Good Idea</h2>
+
+<?php
+    $message = "Hello";
+    $target = "World";
+
+    echo "<h5>$message, $target!</h5>";
+?>
