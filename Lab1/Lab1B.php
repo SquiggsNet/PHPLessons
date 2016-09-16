f87382d1da3c00608ae8d0c5fb3ad580446492e1
Scott Rafael <w0218584@nscc.ca>
2016-09-16 09:13:31 -0300

    Part B established -Step 1 completed

--- Lab1/Lab1B.php
+++ Lab1/Lab1B.php
@@ -8,5 +8,11 @@
     <?php
         echo "<h1>Greetings from Lab1.</h1>";
     ?>
+
+    <p>Here is a section of text added for the purposes of Step 1.5 to display output html text.</p>
+
+    <?php
+        echo "<h3>Inserted another tag!</h3>";
+    ?>
 </body>
 </html>
