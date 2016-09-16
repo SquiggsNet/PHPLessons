fea66f9f57bc979c4686a05b1570e2fb45c06fcb
Scott Rafael <w0218584@nscc.ca>
2016-09-16 09:07:09 -0300

    Part B established -Step 1.2 complete

---
+++ Lab1/Lab1B.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>New PHP Page</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Greetings from Lab1.</h1>";
+    ?>
+</body>
+</html>
