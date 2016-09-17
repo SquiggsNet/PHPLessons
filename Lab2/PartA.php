858897baecc384cc3ff7e2edc2b4a1e923614097
Scott Rafael <w0218584@nscc.ca>
2016-09-17 19:24:02 -0300

    Lab2 - Part A: Step 1 Rev 1

--- Lab2/PartA.php
+++ Lab2/PartA.php
@@ -5,6 +5,8 @@
   <title>Lab 2 - Part A</title>
 </head>
 <body>
+    <h1>Lab 2 - Part A</h1>
+    <h3>Step 1:</h3>
     <?php
         function headingSizes($message, $size)
         {
@@ -18,8 +20,29 @@
 
         for($counter=1;$counter<8;$counter++)
         {
-            headingSizes("This currrent heading size is: $counter", $counter);
+            headingSizes("The currrent heading size is: $counter", $counter);
         }
+    ?>
+    <h3>Step 2:</h3>
+    <?php
+        $message = "Hello, World";
+
+        function justAddBlahValue($passByValue)
+        {
+            $passByValue = "$passByValue...blah";
+        }
+        function justAddBlahReference($passByReference)
+        {
+            $passByValue = "$passByReference...blah";
+        }
+        echo "<p>Default Message: $message</p>";
+        echo "<p>Message by value: ";
+        justAddBlahValue($message);
+        echo "$message</p>";
+        echo "<p>Message by Reference: ";
+        justAddBlahReference($message);
+        echo "$message</p>";
+
     ?>
 </body>
 </html>
