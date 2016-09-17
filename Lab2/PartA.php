6e0dcbe5de7ea7f8ce82cb67719c4615dcf30e81
Scott Rafael <w0218584@nscc.ca>
2016-09-17 18:48:46 -0300

    Lab2 - Part A: Step 1 complete

---
+++ Lab2/PartA.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Lab 2 - Part A</title>
+</head>
+<body>
+    <?php
+        function headingSizes($message, $size)
+        {
+            if ($size > 0 && $size < 7)
+            {
+                echo "<h$size>$message</h$size>";
+            }//end if statement
+        }//end headingSize function
+
+        for($counter=1;$counter<8;$counter++)
+        {
+
+        }
+    ?>
+</body>
+</html>
