f8363952693fd9a422ff6928e2ea3133cf7f4f3b
Scott Rafael <w0218584@nscc.ca>
2016-09-17 18:54:01 -0300

    Lab2 - Part A: Step 1 complete

--- Lab2/PartA.php
+++ Lab2/PartA.php
@@ -11,12 +11,14 @@
             if ($size > 0 && $size < 7)
             {
                 echo "<h$size>$message</h$size>";
-            }//end if statement
+            }else{
+                echo"<p>Sorry, the currently selected heading size is not an option.</p>";
+            }//end if/else statement
         }//end headingSize function
 
         for($counter=1;$counter<8;$counter++)
         {
-
+            headingSizes("This currrent heading size is: $counter", $counter);
         }
     ?>
 </body>
