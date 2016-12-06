e5479ccce26df1c78353e0390cb2a27b17593f1e
Scott Rafael <w0218584@nscc.ca>
2016-12-06 18:08:40 -0400

    Prep for check 2

--- FinalProject/app/Articles.php
+++ FinalProject/app/Articles.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Articles extends Model
 {
     protected $fillable = array('name', 'title', 'page', 'allPages','description', 'contentArea', 'htmlSnippet', 'areas_id', 'pages_id');
+
+    public function pages()
+    {
+        return $this->belongsTo('App\Pages');
+    }
+
+    public function areas()
+    {
+        return $this->belongsTo('App\Areas');
+    }
 }
--- FinalProject/database/migrations/2014_10_12_000000_create_users_table.php
+++ FinalProject/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            //$table->string('first_name');
+            //$table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+//            $table->integer('createdBy');
+//            $table->date('createdDate');
+//            $table->integer('modifiedBy');
+//            $table->date('modifiedDate');
             $table->rememberToken();
             $table->timestamps();
         });
